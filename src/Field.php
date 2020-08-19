2fa738750ec8274bcd93e2cc5fdef343fdb0cc82
Nico Ames <nicholaus.l.ames@gmail.com>
2020-08-19 12:14:28 -0500

    adds editor as extended class

--- src/Field.php
+++ src/Field.php
@@ -138,10 +138,8 @@ class Field extends \craft\base\Field
         $encValue = htmlentities((string)$value, ENT_NOQUOTES, 'UTF-8');
 
         $js = <<<JS
-        import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
-
-ClassicEditor
-    .create(document.getElementById('{$nsId}'), { "plugins": [ PasteFromOffice], 
+Editor
+    .create(document.getElementById('{$nsId}'), {
         "toolbar": {
         "items": ['heading',
                         '|',
--- src/assets/ckeditor/CkeditorAsset.php
+++ src/assets/ckeditor/CkeditorAsset.php
@@ -20,6 +20,7 @@ class CkeditorAsset extends AssetBundle
         $this->sourcePath = dirname(__DIR__, 3).'/lib/ckeditor/dist';
         $this->js = [
             'ckeditor.js',
+            'editor.js'
         ];
 
         parent::init();
